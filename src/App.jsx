7f310e5828386588200385fb17b185ed0c2ee88b
Valentin-arapu <144059053+Valentin-arapu@users.noreply.github.com>
2024-04-09 16:52:07 +0300

    YF-1: I made changes:
    - I uuninstalled bootstrap;
    - I modified the App component.

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,25 @@
 import PharmacyObjects from "./assets/Data.js";
-import 'bootstrap/dist/css/bootstrap.min.css';
-
-
-const renderPharmaceuticalProducts = () => {
-  return (
-    <div>
-      {PharmacyObjects.pharmaceuticals.map((product, id) => (
-        <div key={id} className="text-center">
-          <h3>{product.name}</h3>
-          <p>{product.price}</p>
-          <button>Add to cart</button>
-        </div>
-      ))}
-    </div>
-  )
-}
 
 function App() {
+
+  const renderPharmaceuticalProducts = () => {
+    return (
+      <div>
+        {PharmacyObjects.pharmaceuticals.map((product, id) => (
+          <div key={id}>
+            <h3>{product.name}</h3>
+            <p>{product.price}</p>
+            <button>Add to cart</button>
+          </div>
+        ))}
+      </div>
+    );
+  }
   return (
     <>
-      <h1 className="text-center">Pharmaceutical Products:</h1>
       {renderPharmaceuticalProducts()}
     </>
   );
 }
-
 
 export default App;
